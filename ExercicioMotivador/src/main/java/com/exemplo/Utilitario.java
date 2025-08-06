c988d350240cbbe90b93e8b2d1a69dcc0d990722
pedro-jardim-machado <pedro.machado006@edu.pucrs.br>
2025-08-06 21:30:01 +0000

    Utilitario

---
+++ ExercicioMotivador/src/main/java/com/exemplo/Utilitario.java
@@ -0,0 +1,10 @@
+package com.exemplo;
+
+public class Utilitario extends Carro{
+
+        public Utilitario(TipoCombustivel tipoCombustivel) {
+        super("esportivo", tipoCombustivel, 5, 70);
+    }
+
+
+}
