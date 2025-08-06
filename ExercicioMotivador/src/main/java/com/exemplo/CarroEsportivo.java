1d77bb68aa7153fa3f78d95463912b3cf42517f9
pedro-jardim-machado <pedro.machado006@edu.pucrs.br>
2025-08-06 21:29:53 +0000

    carroEsportivo

---
+++ ExercicioMotivador/src/main/java/com/exemplo/CarroEsportivo.java
@@ -0,0 +1,13 @@
+package com.exemplo;
+
+public class CarroEsportivo extends Carro {
+
+
+
+    public CarroEsportivo(TipoCombustivel tipoCombustivel) {
+        super("esportivo", tipoCombustivel, 6, 45);
+    }
+
+    
+
+}
